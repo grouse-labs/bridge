f6fcefdc38c7b241138504388784665b0d2181cf
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2025-02-10 16:11:03 +1100

    feat(init): setup `init` file for bridge lib

---
+++ init.lua
@@ -0,0 +1,49 @@
+local res = 'bridge'
+local get_res_meta = GetResourceMetadata
+local version, url, des, debug_mode = get_res_meta(res, 'version', 0), get_res_meta(res, 'url', 0), get_res_meta(res, 'description', 0), get_res_meta(res, 'debug_mode', 0) == 'true'
+local load, load_resource_file = load, LoadResourceFile
+local export = exports[res]
+local is_server = IsDuplicityVersion() == 1
+local context = is_server and 'server' or 'client'
+
+---@param bridge CBridge
+---@param module string
+---@return function?
+local function import(bridge, module)
+  local dir = 'modules/'..module..'/'
+  local file = load_resource_file(res, dir..'shared.lua')
+  dir = not file and dir..context..'.lua' or dir
+  file = not file and load_resource_file(res, dir) or file
+  if not file then return end
+  local result, err = load(file, '@@'..res..'/'..dir, 't', _ENV)
+  if not result or err then return error('error occured loading module \''..module..'\''..(err and '\n\t'..err or ''), 3) end
+  bridge[module] = result()
+  if debug_mode then print('^3[bridge]^7 - ^2loaded `bridge` module^7 ^5\''..module..'\'^7') end
+  return bridge[module]
+end
+
+---@param bridge CBridge
+---@param index string
+---@param ... any
+---@return function
+local function call(bridge, index, ...)
+  local module = rawget(bridge, index) or import(bridge, index)
+  if not module then
+    local method = function(...) return export[index](nil, ...) end
+    if not ... then bridge[index] = method end
+    module = method
+  end
+  return module
+end
+
+---@class CBridge
+---@field _VERSION string
+---@field _URL string
+---@field _DESCRIPTION string
+---@field _DEBUG boolean
+---@field require fun(module: string): module Returns the module if it was found and could be loaded. <br> `mod_name` needs to be a dot seperated path from resource to module. <br> Credits to [Lua Modules Loader](http://lua-users.org/wiki/LuaModulesLoader) by @lua-users & ox_lib's [`require`](https://github.com/overextended/ox_lib/blob/cdf840fc68ace1f4befc78555a7f4f59d2c4d020/imports/require/shared.lua#L149).
+local bridge = {_VERSION = version, _URL = url, _DESCRIPTION = des, _DEBUG = debug_mode}
+setmetatable(bridge, {__index = call, __call = call})
+_ENV.bridge = bridge
+
+return bridge
