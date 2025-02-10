c2b97dae8ac108208e9b2f120cffbfb24f14aeb2
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2025-02-10 16:11:26 +1100

    chore: initialise fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author ''
+description ''
+version '0'
+url ''
+
+debug_mode 'true'
+
+shared_script 'init.lua'
+
+files {'modules/**/shared.lua', 'modules/**/client.lua'}
+
+lua54 'yes'
