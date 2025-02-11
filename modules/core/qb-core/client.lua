9c5de2e0594fe6c710ef7d13a15df6a02817692f
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2025-02-11 12:45:35 +1100

    feat(modules/core/qb-core/client): initial client object

---
+++ modules/core/qb-core/client.lua
@@ -0,0 +1,111 @@
+local framework = 'qb-core'
+if framework ~= GetResourceMetadata('bridge', 'framework', 0) then return end
+if not IsResourceValid(framework) then return end
+if IsResourceValid('qbx_core') then return end
+
+local QBCore = exports[framework]:GetCoreObject()
+local PlayerData = {}
+local version = GetResourceMetadata(framework, 'version', 0)
+if version:gsub('%D', '') < ('1.3.0'):gsub('%D', '') then error('incompatible version of '..framework..' detected (expected 1.3.0 or higher, got '..version..')', 0) end
+
+
+---@return 'qb-core'
+local function get_framework() return framework end
+
+---@return string version
+local function get_version() return version end
+
+---@return table QBCore
+local function get_object() return QBCore end
+
+local function get_player()
+  PlayerData = not next(PlayerData) and QBCore.Functions.GetPlayerData() or PlayerData
+  return PlayerData
+end
+
+---@return string|integer identifier The identifier of the `player`.
+local function get_identifier()
+  return get_player().citizenid
+end
+
+---@return string name The name of the `player`.
+local function get_name()
+  return get_player().charinfo.firstname..' '..get_player().charinfo.lastname
+end
+
+---@return {name: string, label: string, grade: number, grade_name: string, grade_label: string, job_type: string, salary: number} job_data The job data of the `player`.
+local function get_job()
+  local data = ConvertJobData(get_player().job)
+  if not data or not next(data) then error('error calling \'getplayerjob\' (job data not found)', 2) end
+  return data
+end
+
+---@return {name: string, label: string, grade: number, grade_name: string} gang_data The gang data for the `player`.
+local function get_gang()
+  local data = ConvertGangData(get_player().gang)
+  if not data or not next(data) then error('error calling \'getplayergang\' (gang data not found)', 2) end
+  return data
+end
+
+---@param groups string|string[] The group(s) to check for. <br> If `groups` is a string, it is the name of the group to check for. <br> If `groups` is a table, it is a list of group names to check for. <br> The group name can be the job name, job label, job type, gang name or gang label.
+---@return boolean has_group Whether the `player` has the specified group(s).
+local function has_group(groups)
+  if type(groups) == 'string' then groups = {groups} end
+  if type(groups) ~= 'table' then error('bad argument #1 to \'hasgroup\' (string or table expected, got '..type(groups)..')', 2) end
+  local player = get_player()
+  local job = ConvertJobData(player.job)
+  local gang = ConvertGangData(player.gang)
+  for i = 1, #groups do
+    local group = groups[i]
+    if job and (job.name == group or job.label == group or job.job_type == group) then return true end
+    if gang and (gang.name == group or gang.label == group) then return true end
+  end
+  return false
+end
+
+---@param money_type 'money'|'cash'|'bank' The type of money to check for. <br> If `money_type` is 'cash', the player's cash is checked. <br> If `money_type` is 'bank', the player's bank is checked.
+---@return integer money The amount of money the `player` has.
+local function get_money(money_type)
+  if type(money_type) ~= 'string' then error('bad argument #2 to \'getplayermoney\' (string expected, got '..type(money_type)..')', 2) end
+  money_type = money_type == 'money' and 'cash' or money_type
+  return get_player().money[money_type:lower()]
+end
+
+--------------------- EVENTS ---------------------
+
+RegisterNetEvent('QBCore:Client:UpdateObject', function()
+  QBCore = exports['qb-core']:GetCoreObject({'Functions', 'Shared'})
+end)
+
+RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
+  PlayerData = QBCore.Functions.GetPlayerData()
+end)
+
+RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
+  PlayerData = {}
+end)
+
+RegisterNetEvent('QBCore:Client:OnJobUpdate', function()
+  PlayerData.job = ConvertJobData(QBCore.Functions.GetPlayerData().job)
+end)
+
+RegisterNetEvent('QBCore:Player:SetPlayerData', function(data)
+  PlayerData = data
+end)
+
+--------------------- OBJECT ---------------------
+
+return {
+  _FRAMEWORK = framework,
+  _VERSION = version,
+  getframework = get_framework,
+  getversion = get_version,
+  getobject = get_object,
+  getplayer = get_player,
+  getplayeridentifier = get_identifier,
+  getplayername = get_name,
+  getplayerjob = get_job,
+  getplayergang = get_gang,
+  doesplayerhavegroup = has_group,
+  getplayermoney = get_money
+}
