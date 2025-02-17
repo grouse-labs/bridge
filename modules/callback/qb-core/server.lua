72040a11686bc293479ced8a0f4fa103fcbf032d
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2025-02-17 20:45:32 +1100

    feat(modules/callback/qb-core/server): initialise base

---
+++ modules/callback/qb-core/server.lua
@@ -0,0 +1,52 @@
+local callback = 'qb-core'
+if callback ~= GetResourceMetadata('bridge', 'callback', 0) then return end
+if not IsResourceValid(callback) then return end
+
+if not IsResourceValid(callback) then return end
+if IsResourceValid('qbx_core') then return end
+
+local QBCore = exports[callback]:GetCoreObject({'Functions', 'Shared'})
+local version = GetResourceMetadata(callback, 'version', 0)
+if version:gsub('%D', '') < ('1.3.0'):gsub('%D', '') then error('incompatible version of '..callback..' detected (expected 1.3.0 or higher, got '..version..')', 0) end
+
+--------------------- FUNCTIONS ---------------------
+
+---@return 'qb-core'
+local function get_callback() return callback end
+
+---@return string version
+local function get_version() return version end
+
+---@return table
+local function get_object() return QBCore end
+
+---@param name string The name of the callback to create.
+---@param cb function The function to call when the callback is triggered.
+local function register_callback(name, cb)
+  if not name or type(name) ~= 'string' then error('bad argument #1 to \'RegisterCallback\' (string expected, got '..type(name)..')', 2) end
+  if not cb or type(cb) ~= 'function' then error('bad argument #2 to \'RegisterCallback\' (function expected, got '..type(cb)..')', 2) end
+  QBCore.Functions.CreateCallback(name, cb)
+end
+
+---@param player integer|string? The `player` to trigger the callback for. <br> If `nil`, the callback will be triggered for the function source.
+---@param name string The name of the callback to trigger.
+---@param cb function The function to call when the callback is received.
+---@param ... any Additional arguments to pass to the callback.
+local function trigger_callback(player, name, cb, ...)
+  if not IsSrcValid(player or source) then error('bad argument #1 to \'TriggerCallback\' (number or string expected, got '..type(player)..')', 2) end
+  if not name or type(name) ~= 'string' then error('bad argument #2 to \'TriggerCallback\' (string expected, got '..type(name)..')', 2) end
+  if not cb or type(cb) ~= 'function' then error('bad argument #3 to \'TriggerCallback\' (function expected, got '..type(cb)..')', 2) end
+  QBCore.Functions.TriggerClientCallback(name, player or source, cb, ...)
+end
+
+--------------------- OBJECT ---------------------
+
+return {
+  _CALLBACK = callback,
+  _VERSION = version,
+  getcallback = get_callback,
+  getversion = get_version,
+  getobject = get_object,
+  register = register_callback,
+  trigger = trigger_callback
+}
