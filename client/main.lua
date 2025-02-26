2f1aaca1e8da3c719eef685c8b54c1611bd4ebae
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2025-02-26 12:11:15 +1100

    fix(client/main): notify events registered for native only

--- client/main.lua
+++ client/main.lua
@@ -1,7 +1,9 @@
-local NOTIFY <const> = GetResourceMetadata('bridge', 'notify', 0)
+local RESOURCE_NAME <const> = GetCurrentResourceName()
+local NOTIFY <const> = GetResourceMetadata(RESOURCE_NAME, 'notify', 0)
 local load, load_resource_file = load, LoadResourceFile
 local dir = 'modules/notify/'..NOTIFY..'/client.lua'
-local notify --[[@type CNotify]] = load(load_resource_file('bridge', dir), '@bridge/'..dir, 't', _ENV)()
+local notify --[[@type CNotify]] = load(load_resource_file(RESOURCE_NAME, dir), '@'..RESOURCE_NAME..'/'..dir, 't', _ENV)()
+if NOTIFY ~= 'native' then return end
 
 --------------------- EVENTS ---------------------
 
