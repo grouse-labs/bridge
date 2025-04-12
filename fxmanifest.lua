5170c19cd54cf54b72c9ed206653259f248518f2
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2025-04-12 18:17:36 +1000

    feat(fxmanifest): add `job_types` to manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -18,6 +18,19 @@ menu 'ox_lib'
 
 notify 'native'
 
+-- If the framework is ESX, the job_types below is used to determine the job type of the player.
+-- Add the job type to the job_types if it is not already present, as well as any additional jobs that should be considered part of that job type.
+job_types 'leo' {
+  ['police'] = true,
+  ['fib'] = true,
+  ['sheriff'] = true
+}
+job_types 'ems' {
+  ['ambulance'] = true,
+  ['doctor'] = true,
+  ['ems'] = true
+}
+
 client_script 'client/main.lua'
 
 files {
