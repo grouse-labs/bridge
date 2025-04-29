4d41d6bc52d105f3673c2efee8de974b11e06b13
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2025-04-30 00:36:51 +1000

    chore(fxmanifest): supporting update for enums

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,15 +9,15 @@ url ''
 -- If the framework is ESX, the job_types below is used to determine the job type of the player.
 -- Add the job type to the job_types if it is not already present, as well as any additional jobs that should be considered part of that job type.
 job_types 'leo' {
-  ['police'] = true,
-  ['fib'] = true,
-  ['sheriff'] = true
+  ['police'] = 1,
+  ['fib'] = 2,
+  ['sheriff'] = 3
 }
 
 job_types 'ems' {
-  ['ambulance'] = true,
-  ['doctor'] = true,
-  ['ems'] = true
+  ['ambulance'] = 1,
+  ['doctor'] = 2,
+  ['ems'] = 3
 }
 
 client_script 'client/main.lua'
